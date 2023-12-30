fbd3a137c01410be02ff186f5e73531bcb4d7cf3
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-31 03:24:05 +0400

    Main page content and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href='' className='logo'>
+          MyBlog
         </a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
       </header>
-    </div>
+      <div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2023/12/ussf-52.jpeg?w=1390&crop=1'
+            alt=''
+          />
+        </div>
+        <div className='text'>
+          <h2>
+            Goodbye mysterious military space plane, and good luck &#8210;
+            wherever you&apos;re going
+          </h2>
+          <p className='info'>
+            <a href='' className='author'>
+              Adnan
+            </a>
+            <time>2023-12-31 03:22</time>
+          </p>
+          <p className="summary">
+            SpaceX&apos;s Falcon Heavy rocket launched a secretive military
+            spaceplane to orbit last night after weeks of delays, though scant
+            details about the mission have been released to the public.
+          </p>
+        </div>
+      </div>
+      <div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2023/12/ussf-52.jpeg?w=1390&crop=1'
+            alt=''
+          />
+        </div>
+        <div className='text'>
+          <h2>
+            Goodbye mysterious military space plane, and good luck &#8210;
+            wherever you&apos;re going
+          </h2>
+          <p className='info'>
+            <a href='' className='author'>
+              Adnan
+            </a>
+            <time>2023-12-31 03:22</time>
+          </p>
+          <p className="summary">
+            SpaceX&apos;s Falcon Heavy rocket launched a secretive military
+            spaceplane to orbit last night after weeks of delays, though scant
+            details about the mission have been released to the public.
+          </p>
+        </div>
+      </div>
+      <div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2023/12/ussf-52.jpeg?w=1390&crop=1'
+            alt=''
+          />
+        </div>
+        <div className='text'>
+          <h2>
+            Goodbye mysterious military space plane, and good luck &#8210;
+            wherever you&apos;re going
+          </h2>
+          <p className='info'>
+            <a href='' className='author'>
+              Adnan
+            </a>
+            <time>2023-12-31 03:22</time>
+          </p>
+          <p className="summary">
+            SpaceX&apos;s Falcon Heavy rocket launched a secretive military
+            spaceplane to orbit last night after weeks of delays, though scant
+            details about the mission have been released to the public.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
