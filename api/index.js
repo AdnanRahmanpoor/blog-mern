b00f7ae0369a27eb3c7e3efa35101babc13cd1e8
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-01-01 14:21:21 +0400

    API, register and login pages + DB connection

--- api/index.js
+++ api/index.js
@@ -1,13 +1,43 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcrypt');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI);
+
+app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
-app.listen(4000);
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  res.json(passOk);
+
+  if (passOk) {
+    // logged in
+  } else {
+    res.status(400).json('Wrong Credentials.');
+  }
+});
+
+app.listen(3001);
