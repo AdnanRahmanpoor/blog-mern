6be5239c5a4098e4d3c966f11cde542232e0dd7c
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-01-01 23:35:47 +0400

    Authetication + encryption

--- api/index.js
+++ api/index.js
@@ -5,11 +5,15 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'abshsjwdbnkefn93rhrf28ho4fn2';
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(cookieParser());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI);
@@ -31,13 +35,31 @@ app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
-  res.json(passOk);
 
   if (passOk) {
     // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
   } else {
     res.status(400).json('Wrong Credentials.');
   }
+});
+
+app.get('/profile', (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
 });
 
 app.listen(3001);
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { UserContext } from '../UserContext';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [redirect, setRedirect] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
 
   async function login(e) {
     e.preventDefault();
 
-    await fetch('http://localhost:3001/login', {
+    const response = await fetch('http://localhost:3001/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
-      headers: {'Content-Type': 'application/json'},
-    })
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
+
+    if (response.ok) {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
+    } else {
+      alert('Wrong Credentials');
+    }
   }
 
+  if (redirect) {
+    return <Navigate to={'/'} />;
+  }
   return (
     <form className='login' onSubmit={login}>
       <h1>Login</h1>
